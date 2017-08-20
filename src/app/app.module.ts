d734c44d7e99115a5c42e6db98571a14e4d7e163
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-08-21 00:58:28 +0100

    basic env set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './base/home/home.component';
+import { HeaderComponent } from './base/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
