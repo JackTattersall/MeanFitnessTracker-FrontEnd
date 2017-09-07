845030f5ea7be46f8c3e3c45563cdbdfa2a337eb
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-09-07 15:22:47 +0100

    adds titalise pipe 2 more tests

--- src/app/shared/pipes/titalise.pipe.spec.ts
+++ src/app/shared/pipes/titalise.pipe.spec.ts
@@ -13,4 +13,13 @@ describe('Titalise without a test bed', () => {
     expect(result).toEqual('Test');
   });
 
+  it('should return input with first words first letter capitalized if more than one word entered', () => {
+    const result = titalise.transform('test my test');
+    expect(result).toEqual('Test my test');
+  });
+
+  it('should return empty string if empty string passed in', () => {
+    const result = titalise.transform('');
+    expect(result).toEqual('');
+  })
 });
