60982834e56fe9aac479b2f29619a1de92e9f849
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-09-11 21:48:43 +0100

    creates account details html and bare bones component

--- src/app/auth/account-details/account-details.component.ts
+++ src/app/auth/account-details/account-details.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
   styleUrls: ['./account-details.component.css']
 })
-export class AccountDetailsComponent implements OnInit {
+export class AccountDetailsComponent {
+
+  emailEditMode = false;
+  passwordEditMode = false;
+
+  emailValue = 'jack@tatts';
+  passwordValue = '*******';
 
   constructor() { }
 
-  ngOnInit() {
+  onEditEmail() {
+    this.emailEditMode = !this.emailEditMode;
+    this.emailValue = '';
+  }
+
+  onEditPassword() {
+    this.passwordEditMode = !this.passwordEditMode;
+    this.passwordValue = '';
+  }
+
+  onEmailNo() {
+    this.emailEditMode = !this.emailEditMode;
+    this.emailValue = 'jack@tatts.com';
+  }
+
+  onPasswordNo() {
+    this.passwordEditMode = !this.passwordEditMode;
+    this.passwordValue = '*******';
+  }
+
+  onEmailYes() {
+    this.emailEditMode = !this.emailEditMode;
+  }
+
+  onPasswordYes() {
+    this.passwordEditMode = !this.passwordEditMode;
   }
 
 }
